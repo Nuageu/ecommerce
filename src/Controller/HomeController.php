34df50357cee2e05904f934559fed3706920ff25
Nuageu <dev.mehdi.benkhelifa@gmail.com>
2022-07-02 17:30:02 +0200

    entity product

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('home.html.twig');
+    }
+}
