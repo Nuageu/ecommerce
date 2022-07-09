1c7fb2819c03a9da32b7e4488b7987cdd7d9866c
Nuageu <dev.mehdi.benkhelifa@gmail.com>
2022-07-09 17:58:39 +0200

    nav bar login/logout sess

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,20 +6,27 @@ use App\Form\LoginType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'security_login', priority: 1)]
-    public function login(AuthenticationUtils $utils): Response
+    public function login(AuthenticationUtils $utils, FormFactoryInterface $factory): Response
     {
         $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
 
+        // $form = $factory->createNamed('', LoginType::class, ['_username' => $utils->getLastUsername()]);
         // dd($utils->getLastAuthenticationError());
 
         return $this->render('security/login.html.twig', [
             'formView' => $form->createView(),
             'error' => $utils->getLastAuthenticationError()
         ]);
+    }
+
+    #[Route('/logout', name: 'security_logout', priority: 1)]
+    public function logout()
+    {
     }
 }
